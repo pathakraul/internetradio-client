0da0552c2a9940eb7c5481d7964e1c6769327609
Rahul Pathak <rpathak@visteon.com>
2016-12-01 15:01:55 +0530

    Removing unnecessary files from public repo

--- stream.pls
+++
@@ -1,6 +0,0 @@
-[playlist]
-numberofentries=1
-File1=http://listen.streamonomy.com/wnjr-radio
-Title1=WNJR-Radio.Com
-Length1=-1
-Version=2
--- visteonproxy.py
+++
@@ -1,3 +0,0 @@
-proxies = {"http": "http://rpathak:LinuxUnix10@proxy.gtm.visteon.com:83",
-           "https": "https://rpathak:LinuxUnix10@proxy.gtm.visteon.com:83",
-          }
